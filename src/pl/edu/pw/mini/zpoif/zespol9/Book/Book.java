2be06631adc83ff45d514278a028c1c39707f570
Alexa263 <olenkakwiatek@gmail.com>
2023-12-16 22:26:46 +0100

    Book&Catalogue + bookData

--- src/pl/edu/pw/mini/zpoif/zespol9/Book/Book.java
+++ src/pl/edu/pw/mini/zpoif/zespol9/Book/Book.java
@@ -1,4 +1,75 @@
 package pl.edu.pw.mini.zpoif.zespol9.Book;
 
+import java.util.Random;
+
 public class Book {
+
+    private Random random = new Random();
+
+    public String title;
+    public String author;
+    public String description;
+    public BookFormat bookFormat;
+    public double bookRating;
+    public int bookRatingCount;
+    public int bookReviewCount;
+    public int numberOfFollowers;
+    public Genre genre;
+    public BookCondition bookCondition;
+
+    public Book(String title, String author, String description, double bookRating, int bookRatingCount, int bookReviewCount, int numberOfFollowers, Genre genre) {
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.bookRating = bookRating;
+        this.bookRatingCount = bookRatingCount;
+        this.bookReviewCount = bookReviewCount;
+        this.numberOfFollowers = numberOfFollowers;
+        this.genre = genre;
+
+        setBookCondition();
+        setBookFormat();
+
+    }
+
+    private void setBookFormat(){
+        double pstwo = random.nextDouble();
+        if (pstwo < 0.4){
+            bookFormat = BookFormat.Softback;
+        } else if (pstwo < 0.7) {
+            bookFormat = BookFormat.Hardcover;
+        } else if (pstwo < 0.9) {
+            bookFormat = BookFormat.Ebook;
+        } else {
+            bookFormat = BookFormat.Clothbound;
+        }
+    }
+
+    private void setBookCondition(){
+        double pstwo = random.nextDouble();
+        if(pstwo < 0.1){
+            bookCondition = BookCondition.AsNew;
+        } else if (pstwo < 0.7) {
+            bookCondition = BookCondition.Good;
+        } else if (pstwo < 0.95){
+            bookCondition = BookCondition.Poor;
+        } else {
+            bookCondition = BookCondition.Damaged;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", description='" + description + '\'' +
+                ", bookFormat=" + bookFormat +
+                ", bookRating=" + bookRating +
+                ", bookRatingCount=" + bookRatingCount +
+                ", bookReviewCount=" + bookReviewCount +
+                ", genre=" + genre +
+                ", bookCondition=" + bookCondition +
+                '}';
+    }
 }
