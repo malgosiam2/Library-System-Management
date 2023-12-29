c6d2f9719732ddb42be65abb0f0e4ed41d61d508
Bogumila Okrojek <01180727@pw.edu.pl>
2023-12-29 19:15:56 +0100

    Reader Class

---
+++ src/pl/edu/pw/mini/zpoif/zespol9/People/Reader.java
@@ -0,0 +1,16 @@
+package pl.edu.pw.mini.zpoif.zespol9.People;
+
+import pl.edu.pw.mini.zpoif.zespol9.Book.Book;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+public class Reader extends Person {
+
+    private List<Book> reservedBooks;
+    private double fine;
+    private Map<Book, LocalDate> checkedOutBooks;
+    private List<Book> toReadBooks;
+
+}
