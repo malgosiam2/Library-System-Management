aaf80f48c19fd0f4beba047ae83ab325dc1268bd
Bogumila Okrojek <01180727@pw.edu.pl>
2023-12-29 19:18:39 +0100

    Librarian Class

---
+++ src/pl/edu/pw/mini/zpoif/zespol9/People/Librarian.java
@@ -0,0 +1,9 @@
+package pl.edu.pw.mini.zpoif.zespol9.People;
+
+import pl.edu.pw.mini.zpoif.zespol9.System.SystemAccess;
+
+public class Librarian extends Person {
+
+    private SystemAccess systemAccess;
+
+}
---
+++ src/pl/edu/pw/mini/zpoif/zespol9/System/SystemAccess.java
@@ -0,0 +1,4 @@
+package pl.edu.pw.mini.zpoif.zespol9.System;
+
+public interface SystemAccess {
+}
