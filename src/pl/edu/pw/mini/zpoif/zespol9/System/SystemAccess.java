01725cb57724cce4934af35b579f4841a2c53e51
Bogumila Okrojek <01180727@pw.edu.pl>
2024-01-03 14:48:44 +0100

    SystemAccess interface

--- src/pl/edu/pw/mini/zpoif/zespol9/System/SystemAccess.java
+++ src/pl/edu/pw/mini/zpoif/zespol9/System/SystemAccess.java
@@ -1,4 +1,10 @@
 package pl.edu.pw.mini.zpoif.zespol9.System;
 
+import pl.edu.pw.mini.zpoif.zespol9.Catalogue.Catalogue;
+import pl.edu.pw.mini.zpoif.zespol9.People.Reader;
+
 public interface SystemAccess {
+    Catalogue getCatalogue();
+    void setCatalogue(Catalogue catalogue);
+    Reader getReader(String login);
 }
