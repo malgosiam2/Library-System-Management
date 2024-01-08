10279dc96d016e03764768eaaa78c2e969f5ac89
Alexa263 <olenkakwiatek@gmail.com>
2024-01-08 10:56:26 +0100

    Catalogue read other buttons

--- src/pl/edu/pw/mini/zpoif/zespol9/Testy/Main.java
+++ src/pl/edu/pw/mini/zpoif/zespol9/Testy/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) {
 
         LibrarySystem librarySystem = new LibrarySystem();
-
         LokigGui lokigGui = new LokigGui(librarySystem);
 
 
