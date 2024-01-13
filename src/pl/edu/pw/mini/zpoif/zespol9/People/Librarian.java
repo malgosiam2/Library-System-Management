887012b18ee89d3cd973f9b3683fbcb3e4874537
Bogumila Okrojek <01180727@pw.edu.pl>
2024-01-13 15:36:24 +0100

    Librarian added empty methods from CheckOutDesk interface

--- src/pl/edu/pw/mini/zpoif/zespol9/People/Librarian.java
+++ src/pl/edu/pw/mini/zpoif/zespol9/People/Librarian.java
@@ -1,10 +1,12 @@
 package pl.edu.pw.mini.zpoif.zespol9.People;
 
 
+import pl.edu.pw.mini.zpoif.zespol9.Book.Book;
+import pl.edu.pw.mini.zpoif.zespol9.System.CheckOutDesk;
 import pl.edu.pw.mini.zpoif.zespol9.System.SystemAccess;
 
 
-public class Librarian extends Person {
+public class Librarian extends Person implements CheckOutDesk {
 
     private SystemAccess systemAccess;
 
@@ -13,4 +15,33 @@ public class Librarian extends Person {
         this.signInData.setLogin("l" + this.signInData.getLogin());
     }
 
+    @Override
+    public void addBook(Book book) {
+
+    }
+
+    @Override
+    public void deleteBook(Book book) {
+
+    }
+
+    @Override
+    public void checkOutBook(String login, Book book) {
+
+    }
+
+    @Override
+    public void acceptBookReturn(String login, Book book) {
+
+    }
+
+    @Override
+    public SignInData addUser(String name, String surname) {
+        return null;
+    }
+
+    @Override
+    public void imposeFine(Reader reader) {
+
+    }
 }
