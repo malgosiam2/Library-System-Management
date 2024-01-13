b5194d833dddf234c3e0716012e89367b7c0090a
Bogumila Okrojek <01180727@pw.edu.pl>
2024-01-13 23:17:36 +0100

    CreatedPasswordAccess changed argument in getNewPassword method

--- src/pl/edu/pw/mini/zpoif/zespol9/System/CreatedPasswordAccess.java
+++ src/pl/edu/pw/mini/zpoif/zespol9/System/CreatedPasswordAccess.java
@@ -1,6 +1,8 @@
 package pl.edu.pw.mini.zpoif.zespol9.System;
 
+import pl.edu.pw.mini.zpoif.zespol9.Exceptions.AlreadySeenThePasswordException;
+
 public interface CreatedPasswordAccess {
 
-    public String getNewPassword(String login);
+    public String getNewPassword() throws AlreadySeenThePasswordException;
 }
