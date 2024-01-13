a6607087b5bf5a82759df358456e267940f05d94
Bogumila Okrojek <01180727@pw.edu.pl>
2024-01-13 17:43:16 +0100

    Librarian completed checkOutBook method

--- src/pl/edu/pw/mini/zpoif/zespol9/People/Librarian.java
+++ src/pl/edu/pw/mini/zpoif/zespol9/People/Librarian.java
@@ -2,8 +2,11 @@ package pl.edu.pw.mini.zpoif.zespol9.People;
 
 
 import pl.edu.pw.mini.zpoif.zespol9.Book.Book;
+import pl.edu.pw.mini.zpoif.zespol9.Book.Status;
 import pl.edu.pw.mini.zpoif.zespol9.System.CheckOutDesk;
 import pl.edu.pw.mini.zpoif.zespol9.System.SystemAccess;
+
+import java.time.LocalDate;
 
 
 public class Librarian extends Person implements CheckOutDesk {
@@ -23,12 +26,14 @@ public class Librarian extends Person implements CheckOutDesk {
 
     @Override
     public void deleteBook(Book book) {
-
+        systemAccess.getCatalogue().getCatalogue().remove(book);
     }
 
     @Override
     public void checkOutBook(String login, Book book) {
-
+        Reader reader = systemAccess.getReader(login);
+        book.status = Status.CheckedOut;
+        reader.getCheckedOutBooks().put(book, LocalDate.now());
     }
 
     @Override
