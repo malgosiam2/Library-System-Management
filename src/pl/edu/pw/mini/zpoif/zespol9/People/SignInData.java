6645d981b08148dfaf5b9d3b1fddf433740b1c37
Bogumila Okrojek <01180727@pw.edu.pl>
2024-01-13 23:18:08 +0100

    SignInData completed getNewPassword method

--- src/pl/edu/pw/mini/zpoif/zespol9/People/SignInData.java
+++ src/pl/edu/pw/mini/zpoif/zespol9/People/SignInData.java
@@ -1,5 +1,6 @@
 package pl.edu.pw.mini.zpoif.zespol9.People;
 
+import pl.edu.pw.mini.zpoif.zespol9.Exceptions.AlreadySeenThePasswordException;
 import pl.edu.pw.mini.zpoif.zespol9.System.CreatedPasswordAccess;
 import pl.edu.pw.mini.zpoif.zespol9.System.PasswordCheckInterfejs;
 
@@ -14,6 +15,7 @@ public class SignInData implements PasswordCheckInterfejs, CreatedPasswordAccess
     private String password;
     private final int id;
     private static int count = 10;
+    private boolean unseen = true;
 
     public SignInData(String name, String surname) {
         this.name = name;
@@ -61,7 +63,11 @@ public class SignInData implements PasswordCheckInterfejs, CreatedPasswordAccess
     }
 
     @Override
-    public String getNewPassword(String login) {
-        return null;
+    public String getNewPassword() throws AlreadySeenThePasswordException {
+        if (unseen){
+            return password;
+        } else{
+            throw new AlreadySeenThePasswordException();
+        }
     }
 }
